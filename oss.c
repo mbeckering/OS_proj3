a369cf4fcafe8494d80f80fe3b2e44e0ae376d30
mbeckering <mikebeckering@gmail.com>
2018-03-02 15:42:37 -0600

    message queues added, critical section implemented
    Started crit sections.
    Added randomized variables for simulated clock time (though work cycles arent randomizing right yet)
    added message queues: one for clock mutex, one for communication to master from slaves.
    consolidated functions that close IPC resources in to one.

--- oss.c
+++ oss.c
@@ -17,24 +17,30 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <time.h>
+#include <sys/msg.h>
 
 #define BILLION 1000000000L
 #define SHMKEY_sim_s 0420423
 #define SHMKEY_sim_ns 0420145
 #define BUFF_SZ sizeof (int)
+#define mutexkey 0420323
+#define commskey 0420541
 
-//function prototype declarations
-void clearShm();
+// Function prototype declarations
+void killchildren();
+void clearIPC();
 static void helpmessage();
 static int setperiodic(double);
 static int setinterrupt();
 static void interrupt(int signo, siginfo_t *info, void *context);
 static void siginthandler(int sig_num);
 
+// GLOBALS
 pid_t childpids[25];
 int maxSlaves = 5; //number of slave processes, default 5
 static FILE *log; //master log file pointer
 int shmid_sim_s, shmid_sim_ns; //shared memory ID holders for sim clock
+int mutex_qid, comms_qid; //message queue id's
 
 /*
  * 
@@ -51,6 +57,27 @@ int main(int argc, char** argv) {
     char str_proclimit[10]; //string arg for exec-ing slaves
     char str_slavenum[10]; //string arg for exec-ing slaves
     pid_t childpid, sh_wpid; //pid holders
+    int totalforks = 0;
+    int localsec;
+    int localns;
+    int temp;
+    
+     //struct for mutex enforcement message queue
+    struct mutexbuf {
+        long mtype;
+        char msgtxt[10];
+    };
+    struct mutexbuf mutexmsg;
+    mutexmsg.mtype = 1;
+    strcpy(mutexmsg.msgtxt, "next!");
+    
+    //struct for communications message queue
+    struct commsbuf {
+        long mtype;
+        pid_t childpid;
+        int s, ns;
+    };
+    struct commsbuf childinfo;
     
      //getopt loop to parse command line options
     while ((option = getopt(argc, argv, "hs:t:l:")) != -1) {
@@ -63,7 +90,7 @@ int main(int argc, char** argv) {
                 sflag = 1;
                 maxSlaves = atoi(optarg);
                 if ( (maxSlaves < 1) || (maxSlaves > 18) ) {
-                    printf("Master: Error: -s range is 1 to 18.\n");
+                    printf("Master: Error: -s range is 1 to 18. ./oss -h for help.\n");
                     exit(0);
                 }
                 break;
@@ -99,7 +126,7 @@ int main(int argc, char** argv) {
     else
         printf("Master: Using default runtime limit of %2.1f seconds.\n", runtime);
     
-    // Set up interrupt handler
+    // Set up interrupt handling
     signal (SIGINT, siginthandler);
     if (setinterrupt() == -1) {
         perror("Failed to set up SIGALRM handler");
@@ -109,6 +136,16 @@ int main(int argc, char** argv) {
     if (setperiodic(runtime) == -1) {
         perror("Failed to setup periodic interrupt");
         return 1;
+    }
+    
+    // Set up message queues
+    if ( (mutex_qid = msgget(mutexkey, 0777 | IPC_CREAT)) == -1 ) {
+        perror("Error generating mutex message queue");
+        exit(0);
+    }
+    if ( (comms_qid = msgget(commskey, 0777 | IPC_CREAT)) == -1 ) {
+        perror("Error generating communication message queue");
+        exit(0);
     }
     
     // Set up shared memory
@@ -126,18 +163,20 @@ int main(int argc, char** argv) {
         }
     int *sim_ns = (int*) shmat(shmid_sim_ns, 0, 0);
     
-    //testing shm
-    *sim_s = 2;
-    printf("Master: sim_s = %d\n", *sim_s);
-    
-    *sim_ns = 1000000;
-    printf("Master: sim_ns = %d\n", *sim_ns);
-    
-    
+    // Initialize sim clock
+    *sim_s = 0;
+    *sim_ns = 0;
     
     //BEGIN MEAT OF PROGRAM*****************************************************
     
-    //while (sim_clock_s < 2)
+    // Push first message into queue to get slaves started
+    if ( msgsnd(mutex_qid, &mutexmsg, sizeof(mutexmsg), 0) == -1 ) {
+        perror("Master: error sending init msg");
+        exit(0);
+    }
+    printf("Master: Initialization message pushed to mutex queue\n");
+    
+    //this for loop execs the first set of children
     for (i=0; i<maxSlaves; i++) {
         if ( (childpid = fork()) < 0 ){ //terminate code
                 perror("Error forking consumer");
@@ -150,20 +189,59 @@ int main(int argc, char** argv) {
             perror("execl() failure"); //report & exit if exec fails
             return 0;
         }
-        printf("Master: Slave forked.\n");
-        //wait for message from terminating child (includes logical slave#?)
-        //critical section: increment simulaton clock, write to log, 
+        childpids[i] = childpid; //store child pid to array
+        totalforks++;
     }
     
+    while (1) {
+        //wait for message from terminating child (includes logical slave)
+        if ( msgrcv(comms_qid, &childinfo, sizeof(childinfo), 1, 0) == -1 ) {
+            perror("Slave: error in msgrcv");
+            exit(0);
+        }
+        //write child termination to log
+        printf("Master: Rcvd child termination pid:%ld, n:%d, ns:%d\n",
+            childinfo.childpid, childinfo.s, childinfo.ns);
+        
+        //critical section 
+        //wait for clock access
+        if ( msgrcv(mutex_qid, &mutexmsg, sizeof(mutexmsg), 1, 0) == -1 ) {
+            perror("Master: error in msgrcv from terminating slave");
+            exit(0);
+        }
+        
+        //check clock. if total+100ns >= 2 seconds, SHUT ER DOWN (break?)
+        localsec = *sim_s;
+        localns = *sim_ns;
+        localns = localns + 100; //increment 100ns for master operation
+        if (localns >= 1000000) { //roll back ns if exceeding 1million
+            localsec++;
+            temp = 1000000 - localns;
+            localns = 100 - temp;
+        }
+        printf("Master: Clock+100=: %d : %d\n", localsec, localns);
+        if (localsec >= 2) {
+            printf("Master: Sim clock limit reached, breaking...\n");
+            break;
+        }
+        //else increment clock by 100ns
+        
+        //cede clock access
+        if ( msgsnd(mutex_qid, &mutexmsg, sizeof(mutexmsg), 0) == -1 ) {
+            perror("Master: error sending init msg");
+            exit(0);
+        }
+        sleep(1);
+        //fork new child and STORE CHILDPID in logical slave# array position
+    }
     
     //END MEAT OF PROGRAM*******************************************************
     
-    //wait for children to finish
-    printf("Master: Waiting for children to finish...\n");
-    while ( (sh_wpid = wait(&sh_status)) > 0);
-
-    //clear shared memory and exit
-    clearShm();
+    //If this point is reached, total runtime has been met
+    //close up shop
+    //clear shared memory and message queues and exit
+    killchildren();
+    clearIPC();
     printf("Master: Normal exit.\n");
     return 1;
 }
@@ -176,14 +254,36 @@ static void helpmessage() {
     exit(0);
 }
 
+//kill the children
+void killchildren() {
+    int sh_status, i;
+    pid_t sh_wpid;
+    printf("Master: Killing children..\n");
+    for (i=0; i < maxSlaves ; i++) {
+        kill(childpids[i], SIGINT);
+    }
+    //wait for all children to finish
+    while ( (sh_wpid = wait(&sh_status)) > 0);
+}
+
 //function to clear shared memory
-void clearShm() {
-    printf("Master: Clearing shared memory...\n");
+void clearIPC() {
+    printf("Master: Clearing IPC resources...\n");
+    //shared memory
     if ( shmctl(shmid_sim_s, IPC_RMID, NULL) == -1) {
         perror("error removing shared memory");
     }
     if ( shmctl(shmid_sim_ns, IPC_RMID, NULL) == -1) {
         perror("error removing shared memory");
+    }
+    //message queues
+    if ( msgctl(mutex_qid, IPC_RMID, NULL) == -1 ) {
+        perror("Master: Error removing mutex_qid");
+        exit(0);
+    }
+    if ( msgctl(comms_qid, IPC_RMID, NULL) == -1 ) {
+        perror("Master: Error removing comms_qid");
+        exit(0);
     }
 }
 
@@ -217,23 +317,9 @@ static int setinterrupt() {
 }
 
 static void interrupt(int signo, siginfo_t *info, void *context) {
-    int sh_status, i;
-    pid_t sh_wpid;
-    printf("master: Timer Interrupt Detected! signo = %d\n", signo);
-    printf("master: Killing children...\n");
-
-    /*
-    for (i=0; i < maxSlaves ; i++) {
-        kill(childpids[i], SIGINT);
-    }
-    */
-    
-    //wait for all children to finish
-    while ( (sh_wpid = wait(&sh_status)) > 0);
-    
-    printf("master: All children terminated\n");
-    clearShm();
-    
+    printf("Master: Timer Interrupt Detected! signo = %d\n", signo);
+    printf("Master: Clearing IPC resources...\n");
+    clearIPC();
     //close log file
     //if (fclose(log) != 0);
     //    perror("Master: Error closing log file");
@@ -246,23 +332,13 @@ static void siginthandler(int sig_num) {
     int sh_status, i;
     pid_t sh_wpid;
     printf("Master: Ctrl+C interrupt detected! signo = %d\n", sig_num);
-    printf("Master: Killing children...\n");
     
-    /*
-    for (i=0; i < maxSlaves; i++) {
-        kill(childpids[i], SIGINT);
-    } 
-    */
-
-    //wait for all children to finish
-    while ( (sh_wpid = wait(&sh_status)) > 0);
+    killchildren();
+    clearIPC();
     
     //if (fclose(log) != 0) ;
     //perror("Master: Error closing log file");
-    
-    printf("Master: All children terminated\n");
-    clearShm();
-    
+
     printf("Master: Terminated: Interrupted\n");
     exit(0);
 }
--- user.c
+++ user.c
@@ -17,10 +17,17 @@
 #include <signal.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
+#include <sys/msg.h>
+#include <sys/types.h>
+#include <string.h>
 
 #define SHMKEY_sim_s 0420423
 #define SHMKEY_sim_ns 0420145
 #define BUFF_SZ sizeof (int)
+#define mutexkey 0420323
+#define commskey 0420541
+
+static void siginthandler(int);
 
 static FILE *log; //master log file pointer
 int shmid_sim_s, shmid_sim_ns; //shared memory ID holders for sim clock
@@ -30,10 +37,47 @@ int shmid_sim_s, shmid_sim_ns; //shared memory ID holders for sim clock
  */
 int main(int argc, char** argv) {
     int slavenum, slavelimit;
-    slavenum = atoi(argv[1]);
-    slavelimit = atoi(argv[2]);
+    slavenum = atoi(argv[1]); //logical number of this process
+    slavelimit = atoi(argv[2]); //max number of concurrent slaves
+    int mutexmsgid, commsqid; //message id for the mutex enforcement queue
+    int local_s, local_ns; //local variables for clock values
+    int worktime; //amount of work done each cycle (randomized in critical section)
+    int temp; //variable swapper
+    int ag_runtime; //aggregate runtime for this slave
     
-    printf("Slave: %d of %d launched.\n", slavenum+1, slavelimit);
+    //select random runtime_limit 1 - 1,000,000
+    unsigned long seed = 3*(int)getpid() + 3*slavenum;
+    srand(seed);
+    unsigned long runtime_limit = rand();
+    runtime_limit <<= 15; //next 4 lines taken from stackoverflow
+    runtime_limit ^= rand();
+    runtime_limit %= 1000000;
+    runtime_limit++;
+    printf("Slave %d: random runtime_limit = %ld\n", slavenum, runtime_limit);
+    
+    //struct for mutex enforcement message queue
+    struct mutexbuf {
+        long mtype;
+        char msgtxt[10];
+    };
+    struct mutexbuf mutexmsg;
+    mutexmsg.mtype = 1;
+    strcpy(mutexmsg.msgtxt, "next!");
+    
+    //struct for communications message queue
+    struct commsbuf {
+        long mtype;
+        pid_t pid;
+        int s, ns;
+    };
+    struct commsbuf myinfo;
+    myinfo.mtype = 1;
+    myinfo.pid = getpid();
+    
+    printf("Slave %d: Launched.\n", slavenum+1, slavelimit);
+    
+    //interrupt handler
+    signal (SIGINT, siginthandler);
     
     // Set up shared memory
     shmid_sim_s = shmget(SHMKEY_sim_s, BUFF_SZ, 0777);
@@ -51,15 +95,97 @@ int main(int argc, char** argv) {
     int *sim_ns = (int*) shmat(shmid_sim_ns, 0, 0);
     
     //testing shm
-    printf("Master: sim_s = %d\n", *sim_s);
+    //printf("Slave %d: sim_s = %d\n", slavenum, *sim_s);
+    //printf("Slave %d: sim_ns = %d\n", slavenum, *sim_ns);
     
-    printf("Master: sim_ns = %d\n", *sim_ns);
+    // Set up message queues
+    int mutexq_id;
+    if ( (mutexq_id = msgget(mutexkey, 0777)) == -1 ) {
+        perror("Slave: Error generating mutex message queue");
+        exit(0);
+    }
     
+    if ( (commsqid = msgget(commskey, 0777 | IPC_CREAT)) == -1 ) {
+        perror("Error generating communication message queue");
+        exit(0);
+    }
     
+    // The Business Loop********************************************************
+    // (change to while counter < runtim)
+    while (1) {
+        //barrier to enter critical section
+        if ( msgrcv(mutexq_id, &mutexmsg, sizeof(mutexmsg), 1, 0) == -1 ) {
+            perror("Slave: error in msgrcv");
+            exit(0);
+        }
+        //critical section: pull clock values first
+        local_s = *sim_s;
+        local_ns = *sim_ns;
+        //cede and break if total master runtime_limit has been reached
+        if (local_s >= 2) {
+            printf("Slave %d: total master runtime limit reached, ceding...\n", slavenum);
+            if ( msgsnd(mutexq_id, &mutexmsg, sizeof(mutexmsg), 0) == -1 ) {
+                perror("Slave: error exiting crit section");
+            exit(0);
+            }
+            break;
+        }
+        //generate random work time
+        srand(seed);
+        worktime = rand() %20000 + 1;
+        printf("Slave %d: Tryna work %d ns.\n", slavenum, worktime);
+        //if this worktime will exceed my slave runtime limit
+        if (ag_runtime + worktime >= runtime_limit) {
+            temp = runtime_limit - ag_runtime; //remainder of time before my limit
+            ag_runtime = ag_runtime + temp; //total time i worked
+            local_ns = local_ns + temp; //increment ns by the time I was allowed to work before cutoff
+            //send message to master that I've finished
+            myinfo.s = local_s;
+            myinfo.ns = local_ns;
+            if ( msgsnd(commsqid, &myinfo, sizeof(myinfo), 0) == -1 ) {
+                perror("Slave: Error sending termination message");
+                exit(0);
+            }
+            //cede the crit section before exiting
+            if ( msgsnd(mutexq_id, &mutexmsg, sizeof(mutexmsg), 0) == -1 ) {
+                printf("Slave %d:", slavenum);
+                perror("error exiting crit section");
+            exit(0);
+        }
+            break;//break out and terminate this process
+        }
+        
+        local_ns = local_ns + worktime; //increment local clock variable
+        if (local_ns >= 1000000) { //roll back ns if exceeding 1million
+            local_s++;
+            temp = 1000000 - local_ns;
+            local_ns = worktime - temp;
+        }
+        printf("Slave %d: clock now at %d : %d\n", slavenum, local_s, local_ns);
+        //update sim clock in chared memory
+        *sim_s = local_s;
+        *sim_ns = local_ns;
+        
+        //end critical section and cede clock access
+        if ( msgsnd(mutexq_id, &mutexmsg, sizeof(mutexmsg), 0) == -1 ) {
+            printf("Slave %d:", slavenum);
+            perror("error exiting crit section");
+            exit(0);
+        }
+        sleep(1);
+    }
     
-    sleep(1);
+    // End Business Loop********************************************************
+    
     printf("Slave: %d of %d normal exit.\n", slavenum+1, slavelimit);
-
     return 1;
+}
+
+//signal handler
+static void siginthandler(int sig_num) {
+    int sh_status, i;
+    pid_t sh_wpid;
+    printf("Slave(pid %ld) Terminating: Interrupted.\n", getpid());
+    exit(0);
 }
 
